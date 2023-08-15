bd4a81f2f53856b844444e2ff2715fdf7bfa81b0
Jason <jasonetienne1@gmail.com>
2023-08-15 18:10:43 +0100

    sad

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 // Add tasks to an array
 let tasks = [];
 
+let nextTaskId = 0; // A counter for assigning unique IDs to tasks
+
 // function that takes the users input and adds it it that task aray
 function addTask(){
     // grab the user input from the element with the ID 'taskInput'
@@ -9,6 +11,7 @@ function addTask(){
     if (taskInput.trim() !== ""){ // check that input is not empty
         // Create an object for the tasks added 
         const task ={
+            id: nextTaskId++, // Assign a unique ID to this task
             text: taskInput,
             done: false
         };
@@ -21,6 +24,8 @@ function addTask(){
         // Call this after adding the task to the array to display tasks
         displayTasks(); 
     }
+
+    console.log(tasks);
 }
 
 
@@ -42,9 +47,46 @@ function displayTasks(){
     for (let task of tasks){
         // Create a new list item element for each task
         let listItem = document.createElement('li');
-        // Set the text content of the list item to the 'text' property of the task
-        listItem.textContent = task.text;
+
+        // Create a span element to hold the task text
+        let taskSpan = document.createElement('span');
+        taskSpan.textContent = task.text;
+
+        // If the task is done, strike through the text
+        if (task.done){
+            taskSpan.style.textDecoration = "line-through";
+        }
+
+        // Create a "Done" button for each task
+        let doneButton = document.createElement('button');
+        doneButton.textContent = "Done";
+
+        // Add a click event listener to mark the task as done
+        doneButton.addEventListener('click', function(){
+            markTaskAsDone(task.id); // Pass the unique ID of the clicked task
+        });
+
+        // Append the span and the button to the list item
+        listItem.appendChild(taskSpan);
+        listItem.appendChild(doneButton);
+
         // Append the list item to the task list in the HTML
         taskList.appendChild(listItem);
     }
 }
+
+
+function markTaskAsDone(taskId){
+    // Find the task with the given ID ans set its "done" property to True
+    for (let task of tasks){
+        if (task.id === taskId){
+            task.done = true;
+            break;
+        }
+    }
+
+    displayTasks(); // Update the display to reflect the change
+
+}
+
+//asd
